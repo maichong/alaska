8cbbedcc2a04153c607bc3288d7bb9d0c7e901ca
Liang Xingchen <liang@maichong.it>
2016-04-13 13:02:17 +0800

    0.8.1

--- src/service/loadSleds.js
+++ src/service/loadSleds.js
@@ -18,4 +18,24 @@ export default async function loadSleds() {
   this.debug('loadSleds');
 
   this._sleds = util.include(this.dir + '/sleds', true, { alaska, service }) || {};
+
+  for (let name in this._sleds) {
+    let Sled = this._sleds[name];
+    //加载扩展配置
+    for (let dir of this._configDirs) {
+      let file = dir + '/sleds/' + name + '.js';
+      if (util.isFile(file)) {
+        let ext = util.include(file, false, { alaska, service });
+        if (ext.pre) {
+          Sled.pre(ext.pre);
+        }
+        if (ext.post) {
+          Sled.post(ext.post);
+        }
+        if (ext.default) {
+          ext.default(Sled);
+        }
+      }
+    }
+  }
 };
