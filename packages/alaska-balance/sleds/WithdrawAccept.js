63560433fa594643e0ccf4ddf3ab8a1221ac3c49
Liang Xingchen <liang@maichong.it>
2017-01-17 12:26:57 +0800

    sled data => params

--- packages/alaska-balance/sleds/WithdrawAccept.js
+++ packages/alaska-balance/sleds/WithdrawAccept.js
@@ -4,11 +4,11 @@ import { Sled } from 'alaska';
 import Withdraw from '../models/Withdraw';
 
 export default class WithdrawAccept extends Sled {
-  async exec(data: {
+  async exec(params: {
     ctx?:Alaska$Context;
     withdraw:Withdraw;
   }): Promise<Object> {
-    let withdraw: Withdraw = data.withdraw;
+    let withdraw: Withdraw = params.withdraw;
     if (withdraw.state === 0) {
       withdraw.state = 1;
       await withdraw.save();
