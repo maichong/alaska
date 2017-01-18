9d135c59bdbd0c46db1dc3470c15de64c784b525
庞金梁 <pang@maichong.it>
2017-01-18 18:16:25 +0800

    alaska-queue

---
+++ packages/alaska-queue-redis/index.js
@@ -0,0 +1,79 @@
+// @flow
+
+const redis = require('redis');
+
+export default class RedisQueueDriver {
+  key: string;
+  options: Object;
+  isQueueDriver: boolean;
+  _driver: any;
+  constructor(options: Object) {
+    this.key = options.key;
+    this.options = options;
+    this.isQueueDriver = true;
+    this._driver = redis.createClient(options);
+  }
+
+  /**
+   * [async] 将元素插入队列
+   * @param {*} item
+   */
+  push(item: any) {
+    return new Promise((resolve, reject)=> {
+      this._driver.rpush(this.key, JSON.stringify(item), function (error) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+
+  /**
+   * [async] 读取队列中的元素
+   * @param {number} timeout 超时时间,单位毫秒,默认不阻塞,为0则永久阻塞
+   * @returns {*}
+   */
+  pop(timeout: number): any {
+    let method = timeout === undefined ? 'lpop' : 'blpop';
+    return new Promise((resolve, reject)=> {
+      let args = [this.key];
+      if (method === 'blpop') {
+        args.push(parseInt(timeout / 1000));
+      }
+      args.push(function (error, res) {
+        if (error) {
+          reject(error);
+        } else {
+          if (res !== null) {
+            try {
+              if (Array.isArray(res)) {
+                res = res[1];
+              }
+              res = JSON.parse(res);
+            } catch (error) {
+              res = null;
+            }
+          }
+          resolve(res);
+        }
+      });
+      this._driver[method].apply(this._driver, args);
+    });
+  }
+
+  /**
+   * 释放当前所有任务,进入空闲状态
+   */
+  free() {
+  }
+
+  /**
+   * 销毁队列
+   */
+  destroy() {
+    this._driver.quit();
+    this._driver = null;
+  }
+}
