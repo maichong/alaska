242eeac247eb60546f0c373a77825c8725e98c51
ZhaoLei <zhao@maichong.it>
2017-01-18 16:43:54 +0800

    flow alaska-payment-balance

--- packages/flow-alaska/alaska-payment.js
+++ packages/flow-alaska/alaska-payment.js
@@ -2,22 +2,23 @@
 
 declare module 'alaska-payment' {
   declare class PaymentService extends Alaska$Service {
-  constructor(options?: Alaska$Service$options):void;
-  payments: Object;
+    constructor(options?: Alaska$Service$options):void;
+    payments: Object;
   }
   declare var exports: PaymentService;
 }
 declare module 'alaska-payment/models/Payment' {
   declare class Payment extends Alaska$Model {
-  title: string;
-  user: User;
-  amount: number;
-  type: Object;
-  params: Object;
-  state: Object;
-  failure: string;
-  createdAt: Date;
-  preSave():void;
+    title: string;
+    user: User;
+    amount: number;
+    type: Object;
+    params: Object;
+    state: number;
+    failure: string;
+    createdAt: Date;
+    currency:string;
+    preSave():void;
   }
   declare var exports: Class<Payment>;
 }
