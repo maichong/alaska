f908705d3ca65d24a1e9fdb620ac700b706a741d
庞金梁 <pang@maichong.it>
2017-01-21 16:38:42 +0800

    sidebar

--- packages/alaska-admin-view/views/Sidebar.jsx
+++ packages/alaska-admin-view/views/Sidebar.jsx
@@ -1,8 +1,4 @@
-/**
- * @copyright Maichong Software Ltd. 2016 http://maichong.it
- * @date 2016-02-29
- * @author Liang <liang@maichong.it>
- */
+// @flow
 
 import React from 'react';
 import Menu from './Menu';
@@ -23,28 +19,30 @@ export default class Sidebar extends React.Component {
     router: object
   };
 
-  constructor(props) {
+  state:Object;
+
+  constructor(props:Object) {
     super(props);
     this.state = {
       menu: ''
     };
   }
 
-  handleChange = (item) => {
+  handleChange = (item:Object) => {
     this.context.router.push(item.link);
     this.setState({ menu: item.id });
   };
 
   render() {
     let { menu, layout } = this.props;
-    if (layout == 'hidden') {
-      return <div></div>;
+    if (layout === 'hidden') {
+      return <div />;
     }
     return (
       <Node id="sidebar">
         <Node id="sidebarInner">
-          <Logo/>
-          <Menu items={menu} layout={layout} value={this.state.menu} onChange={this.handleChange}/>
+          <Logo />
+          <Menu items={menu} layout={layout} value={this.state.menu} onChange={this.handleChange} />
           <Copyright layout={layout} />
         </Node>
       </Node>
