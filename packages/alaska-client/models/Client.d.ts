348e09033f51b2ed7222a113fca861b567e56b12
chaorong <chaorong@maichong.it>
2018-11-24 14:16:08 +0800

    alaska-client d.ts

--- packages/alaska-client/models/Client.d.ts
+++ packages/alaska-client/models/Client.d.ts
@@ -3,6 +3,9 @@ import { Model } from 'alaska-model';
 declare class Client extends Model {
   id: string;
   token: string;
+  deviceId: string;
+  platform: string;
+  user: string;
 }
 
 export default Client;
