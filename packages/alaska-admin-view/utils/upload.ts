890bc8b8e4f16e5549e7fb453dafe1f1980c3ace
Liang Xingchen <liang@maichong.it>
2018-12-01 19:45:36 +0800

    Update libs & upload bug

--- packages/alaska-admin-view/utils/upload.ts
+++ packages/alaska-admin-view/utils/upload.ts
@@ -2,7 +2,7 @@ import { UploadOptions, UploadResult } from '..';
 import api from './api';
 
 export default function upload(options: UploadOptions): Promise<UploadResult> {
-  return api.upload('/upload', {
+  return api.post('/upload', {
     query: {
       _model: options.model,
       _field: options.field,
