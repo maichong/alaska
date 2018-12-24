aff73f99b4d9505a4b510870626f98f838383880
Liang Xingchen <liang@maichong.it>
2018-12-24 19:09:27 +0800

    Menu 展开bug

--- packages/alaska-admin-view/views/Menu.tsx
+++ packages/alaska-admin-view/views/Menu.tsx
@@ -1,40 +1,28 @@
 import * as _ from 'lodash';
 import * as React from 'react';
 import Node from './Node';
-import { MenuProps, StoreState, Menu as MenuType } from '..';
+import { MenuProps, Settings, StoreState } from '..';
 import MenuItem from './MenuItem';
 import { connect } from 'react-redux';
 
 interface Props extends MenuProps {
-  locale: string;
-  superMode: boolean;
-  abilities: {
-    [key: string]: boolean;
-  }
+  settings: Settings;
 }
 interface MenuState {
-  _superMode?: boolean;
-  _menus?: any;
   opendId: string;
-  menus: MenuType[];
 }
 
 class Menu extends React.Component<Props, MenuState> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      opendId: '',
-      menus: []
+      opendId: ''
     };
   }
 
   static getDerivedStateFromProps(nextProps: Props, prevState: MenuState) {
-    let nextState: Partial<MenuState> = {
-      _menus: nextProps.menus,
-      _superMode: nextProps.superMode
-    };
     if (!nextProps.opened && prevState.opendId) {
-      nextState.opendId = '';
+      return { opendId: '' };
     }
 
     let { menus } = nextProps;
@@ -42,16 +30,10 @@ class Menu extends React.Component<Props, MenuState> {
     if (path) {
       let menu = _.find(menus, (menu) => path === menu.link);
       if (menu) {
-        nextState.opendId = menu.id;
+        return { openId: menu.id };
       }
     }
-    if (nextProps.superMode !== prevState._superMode || menus !== prevState._menus) {
-      nextState.menus = _.filter(menus, (item) => (
-        (!item.ability || nextProps.abilities[item.ability])
-        && (!item.super || nextProps.superMode)
-      ))
-    }
-    return nextState;
+    return null;
   }
 
   handleClick = (menuId: string, opened: boolean) => {
@@ -67,8 +49,8 @@ class Menu extends React.Component<Props, MenuState> {
   }
 
   render() {
-    const { level, layout, opened, onChange } = this.props;
-    const { opendId, menus } = this.state;
+    const { level, menus, layout, opened, onChange, settings } = this.props;
+    const { opendId } = this.state;
     return (
       <Node
         className={`menu menu-${level}`}
@@ -77,20 +59,24 @@ class Menu extends React.Component<Props, MenuState> {
         tag="ul"
       >
         {
-          _.map(menus, (menu) => <MenuItem
-            key={menu.id}
-            openId={opendId}
-            opened={opened}
-            onClick={this.handleClick}
-            onChange={onChange}
-            menu={menu}
-            layout={layout}
-            level={level}
-          />)
+          _.map(menus, (menu) => {
+            if (menu.ability && !settings.abilities[menu.ability]) return null;
+            if (menu.super && !settings.superMode) return null;
+            return (<MenuItem
+              key={menu.id}
+              openId={opendId}
+              opened={opened}
+              onClick={this.handleClick}
+              onChange={onChange}
+              menu={menu}
+              layout={layout}
+              level={level}
+            />);
+          })
         }
       </Node>
     );
   }
 }
 
-export default connect(({ settings }: StoreState) => ({ locale: settings.locale, superMode: settings.superMode, abilities: settings.abilities }))(Menu);
+export default connect(({ settings }: StoreState) => ({ settings }))(Menu);
