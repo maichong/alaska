3f8266e2d24b47f2b1f5603eb2012d1d5205bf9a
Liang Xingchen <liang@maichong.it>
2018-12-28 00:13:21 +0800

    库存记录关联

--- packages/alaska-inventory/plugins/alaska-goods/models/Goods.ts
+++ packages/alaska-inventory/plugins/alaska-goods/models/Goods.ts
@@ -10,4 +10,12 @@ export default {
       needRecords: 1
     }
   },
+  relationships: {
+    inventoryList: {
+      title: 'Inventory History',
+      ref: 'alaska-inventory.Inventory',
+      path: 'goods',
+      protected: true
+    }
+  }
 };
