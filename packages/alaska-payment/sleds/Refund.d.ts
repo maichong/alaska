6d06a35ea1feb2869940e6c9ed55cd2323f080ed
lulu <lulu@maichong.it>
2019-02-14 18:34:11 +0800

    Refund sled type file

---
+++ packages/alaska-payment/sleds/Refund.d.ts
@@ -0,0 +1,5 @@
+import { Sled } from 'alaska-sled';
+import RefundModel from '../models/Refund';
+import { RefundParams } from '..';
+
+export default class Refund extends Sled<RefundParams, RefundModel> { }
